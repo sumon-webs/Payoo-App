77d5cc8127fc1f75c874d3df8557768da89b53d3
sumon-webs <sumon01333581557@gmail.com>
2026-02-18 23:15:31 +0600

    Numbering develop

--- script/login.js
+++ script/login.js
@@ -1,6 +1,11 @@
 document.getElementById("login-btn").addEventListener("click", function () {
     const inputNumber = document.getElementById("input-number");
     const number = inputNumber.value;
+    console.log(number.startsWith("01"));
+    if(number.startsWith('01') === false){
+        alert("Number must start with 01")
+        return
+    }
 
     const inputPin = document.getElementById("input-pin");
     const pin = inputPin.value;
@@ -8,7 +13,7 @@ document.getElementById("login-btn").addEventListener("click", function () {
     inputNumber.value = ''
     inputPin.value = ''
 
-    if (number.toString().length === 11 && number.startsWith('01')) {
+    if (number.toString().length === 11) {
         alert("Log in success")
         window.location.assign("./home.html")
     }
